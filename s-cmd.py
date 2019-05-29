c2288e381018c9bfac70594a29bbc0cc25453aa7
naihsi <naihsi@gmail.com>
2019-05-29 23:38:57 +0000

    add list

--- s-cmd.py
+++ s-cmd.py
@@ -1,5 +1,20 @@
 
+import boto3
+from pprint import pprint
+
 # #### ##### ##### #####
+
+
+def ec2():
+    client = boto3.client('ec2', region_name='us-west-2')
+    return client
+
+
+def list_inst():
+    _filters = []
+    _res = ec2().describe_instances(Filters=_filters)
+
+    pprint(_res)
 
 
 # #### ##### ##### #####
@@ -7,6 +22,7 @@
 
 def test():
     print("init")
+    list_inst()
 
 
 if __name__ == "__main__":
